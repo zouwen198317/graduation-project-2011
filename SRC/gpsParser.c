02ccd06ae8b7851f156b2424083ce9564ed24487
michael.behman <michael.behman@gmail.com>
2011-02-09 06:51:12 +0000

    Minor tweeks to the gpsParser code, and adding comments.

--- SRC/gpsParser.c
+++ SRC/gpsParser.c
@@ -17,63 +17,123 @@
  */
 
 
-
+/* Header files. */
 #include "gpsParser.h"
 
+/* Functions' Prototypes. */
+char ** __fieldsSeparator( char * textString, char * fields[13]);
 
+/* Functions' Implementation. */
+
+/* NMEARead (explained in the header file.) */
 GPSData * NMEARead( char * NMEAstring, GPSData * GPSDataPointer)
 {
 	char *		tempChar = NMEAstring;
 	struct tm	timestruct;
 	char *		fields[13];
 
-	if( !fieldsSperator( NMEAstring, fields ) || strncmp( fields[0], "$GPRMC", 6 ) )
-	{
+	/* Initialization of GPSData struct. */
+	*GPSDataPointer = ( GPSData ) { 0, false, 0, 0, 0 };
+
+	/* Returning NULL, if data string is invalid. */
+	if( !__fieldsSeparator( NMEAstring, fields ) ||
+			strncmp( fields[0], "$GPRMC", 6 ) ||
+			( fields[ 2 ][ 0 ] != 'A' &&
+			  ( strlen( fields[ 1 ] ) != 10 ||
+			    strlen( fields[ 9 ] ) != 6 ) ) )
 		return NULL;
-	}
-	else if ( fields[ 2 ][ 0 ] != 'A' && ( strlen( fields[ 1 ] ) != 10 || strlen( fields[ 9 ] ) != 6 ) )
-	{
-		GPSDataPointer->valid = false;
+
+	/* Parsing time and date.
+	 * The time is stored in field 1 while the date is stored in field9.
+	 * Each digit is substracted by 48 to convert from char to int, then the
+	 * tens digits are multiplied by ten and then added to the units.
+	 */
+	timestruct.tm_hour  =	( fields[ 1 ][ 0 ] - 48 ) * 10 +
+		fields[ 1 ][ 1 ] - 48;
+	timestruct.tm_min   = 	( fields[ 1 ][ 2 ] - 48 ) * 10 +
+		fields[ 1 ][ 3 ] - 48;
+	timestruct.tm_sec   = 	( fields[ 1 ][ 4 ] - 48 ) * 10 +
+		fields[ 1 ][ 5 ] - 48;
+	timestruct.tm_mday  =	( fields[ 9 ][ 0 ] - 48 ) * 10 +
+		fields[ 9 ][ 1 ] - 48;
+	/* tm_mon stores months from 0 to 11, hence an extra 1 is subtracted.*/
+	timestruct.tm_mon   = 	( fields[ 9 ][ 2 ] - 48 ) * 10 +
+		fields[ 9 ][ 3 ] - 49;
+	/* tm_year is the number of years since 1900, hence a 100 is added to
+	 * convert to 20XX.
+	 */
+	timestruct.tm_year  =	( fields[ 9 ][ 4 ] - 48 ) * 10 +
+		fields[ 9 ][ 5 ] + 52;
+	/* tm_isdst set to '-1' meaning that the usage of DST is unknown. */
+	timestruct.tm_isdst =	-1;
+	/* Time is converted to time_t and store in GPSDataPointer. */
+	GPSDataPointer->timeAndDate = mktime(&timestruct);
+	
+	/* If the data are invalid, a NULL is returned with only the time and
+	 * date set in GPSDataPointer.
+	 */
+	if( fields[2][0] != 'A' )
 		return NULL;
-	}
-	else
-	{
-		//Parsing time and date.
-		timestruct.tm_hour  =	( fields[ 1 ][ 0 ] - 48 ) * 10 + fields[ 1 ][ 1 ] - 48;
-		timestruct.tm_min   = 	( fields[ 1 ][ 2 ] - 48 ) * 10 + fields[ 1 ][ 3 ] - 48;
-		timestruct.tm_sec   = 	( fields[ 1 ][ 4 ] - 48 ) * 10 + fields[ 1 ][ 5 ] - 48;
-		timestruct.tm_mday  =	( fields[ 9 ][ 0 ] - 48 ) * 10 + fields[ 9 ][ 1 ] - 48;
-		timestruct.tm_mon   = 	( fields[ 9 ][ 2 ] - 48 ) * 10 + fields[ 9 ][ 3 ] - 49;
-		timestruct.tm_year  =	( fields[ 9 ][ 4 ] - 48 ) * 10 + fields[ 9 ][ 5 ] + 52;
-		timestruct.tm_isdst =	-1;
-		GPSDataPointer->timeAndDate = mktime(&timestruct);
-		
-		if( fields[2][0] != 'A' )
-		{
-			GPSDataPointer->valid = false;
-			return NULL;
-		}
-		else
-		{
-			GPSDataPointer->valid = true;
-			GPSDataPointer->lat = ( ( fields[ 3 ][ 0 ] - 48 ) * 10 + fields[ 3 ][ 1 ] - 48 + atof( fields[ 3 ] + 2 ) / 60 ) * ( ( fields[ 4 ][ 0 ] == 'N' ) ? 1 : -1 );
-			GPSDataPointer->lon = ( ( fields[ 5 ][ 0 ] - 48 ) * 100 + ( fields[ 5 ][ 1 ] - 48 ) * 10 + fields[ 5 ][ 2 ] - 48 + atof( fields[ 5 ] + 3 ) / 60 ) * ( ( fields[ 6 ][ 0 ] == 'E' ) ? 1 : -1 );
-			GPSDataPointer->speed = atof( fields[ 7 ] ) * 1.852 ;
-			return GPSDataPointer;
-		}
-	}
+
+	GPSDataPointer->valid = true;
+
+	/* The rest of the data is parsed and converted. */
+
+	GPSDataPointer->lat = ( ( fields[ 3 ][ 0 ] - 48 ) * 10 +
+			fields[	3 ][ 1 ] - 48 + atof( fields[ 3 ] + 2 )
+			/ 60 ) * ( ( fields[ 4 ][ 0 ] == 'N' ) ? 1:-1);
+
+	GPSDataPointer->lon = ( ( fields[ 5 ][ 0 ] - 48 ) * 100 +
+			( fields[ 5 ][ 1 ] - 48 ) * 10 +
+			fields[ 5 ][ 2 ] - 48 +
+			atof( fields[ 5 ] + 3 ) / 60 ) *
+		( ( fields[ 6 ][ 0 ] == 'E' ) ? 1 : -1 );
+
+	/* Speed is converted from knots to km/h by multiplying by 1.852 */
+	GPSDataPointer->speed = atof( fields[ 7 ] ) * 1.852 ;
+
+	return GPSDataPointer;
+	
 }
 
 
-char ** fieldsSperator( char * textString, char * fields[13])
+/* __fieldsSeparator:
+ * It is used to separate the fields in the data string sent. It takes two
+ * parameters; The first is a char pointer containing the string to be parsed,
+ * the second is an array of char pointers to store the separated fields. Fields
+ * are comma separated.
+ * Return value:
+ * 	+ If the number of fields in the data string is less than 13, it returns
+ * 	  a NULL pointer.
+ *	+ On success, it returns a pointer to array of fields given in the
+ *	  second parameter.
+ */
+char ** __fieldsSeparator( char * textString, char * fields[13])
 {
+	/* Main loop; Twelve iterations for twelve commas separating between
+	 * thirteen fields.
+	 * Note: At the beginning textString points to the beginning of the
+	 * 	 first field (the beginning of the whole data string).
+	 */
 	for( int i = 0; i < 12; i++)
 	{
+		/* Current field begins with textString. */
 		fields[ i ] = textString;
+
+		/* Looking for the next comma and assigning its position to
+		 * textString.
+		 */
 		if( !( textString = strchr( textString, ',') ) )
-			return NULL;
+			return NULL; /* returnin NULL when no comma found. */
+
+		/* Replacing the comma by a '\0', to indicate the end of the
+		 * current field, and increasing textString by one.
+		 */
 		*(textString++) = '\0';
 	}
+
+	/* Assigning last field. */
 	fields[ 12 ] = textString;
+
 	return fields;
 }
