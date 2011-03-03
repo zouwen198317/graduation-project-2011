cf34d7bb9f32d4fe8d4953e9f2fd697004982698
michael.behman@gmail.com <michael.behman@gmail.com@19449c1d-5a4a-c5cd-6ffb-47ce3f5ddbbb>
2011-03-03 19:57:11 +0000

    Logger is implemented. Not totally mature yet, but it logs. A critical bug in test/Makefile was fixed, where it didn't compile correctly if a dependency variable had more than one library. test_gpsProcess was taken a bit further to fork and use the logger. Directory SAMPLES/ was moved to test_bin/extra/.

---
+++ trunk/test/test_logger.c
@@ -0,0 +1,54 @@
+/*
+ * The logger testing code.
+ * Copyright (C) 2011  Michael M. Behman (michael.behman@gmail.com)
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+
+/* Header files. */
+#include "../SRC/logger.h"
+
+/* Definitions. */
+#define LOGFILENAME	"extra/logfile"
+
+/* Marcros. */
+#define LOG( x )		log_write( "test_log", x );
+
+/* Functions. */
+int main()
+{
+	int ret;
+
+	if( ret = log_init( LOGFILENAME ) )
+	{
+		printf("Error opening log file: %s\n", strerror( ret ) );
+		exit( EXIT_FAILURE );
+	}
+	
+	printf("Log file opened successfully.\n");
+	LOG( "First log." );
+	LOG( "Second log." );
+	printf("Logs written.\n");
+	LOG( "Closing log." );
+
+	if( ret = log_term() )
+	{
+		LOG( "Cannot close log." );
+		printf( "Error closing log: %s\n", strerror( ret ) );
+	}
+	printf( "Log file closed successfully.\n");
+	return 0;
+}
+
